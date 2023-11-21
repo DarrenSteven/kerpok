7e3e06b632294675c880cb44b712bd5a3db8e55d
Darren Steven <darren.steven@itbss.ac.id>
2023-11-21 11:01:30 +0700

    Menambahkan latihan 1

---
+++ latihan1/SupermarketSystem.java
@@ -0,0 +1,27 @@
+class PurchaseException extends Exception {
+    public PurchaseException(String message) {
+        super(message);
+    }
+}
+
+public class SupermarketSystem {
+    public int kodeBarang, hargaBeli, hargaJual;
+    public String namaBarang, gudang;
+
+    public void processPurchase(int beli, int jual) throws PurchaseException {
+        if (jual <= 0 && beli < 1) {
+            throw new PurchaseException("Terjadi Kesalahan: Harga jual harus lebih dari 0 dan jumlah pembelian minimal 1");
+        }
+        System.out.println("Pembelian berhasil diproses.");
+    }
+
+    public static void main(String[] args) {
+        SupermarketSystem supermarket = new SupermarketSystem();
+
+        try {
+            supermarket.processPurchase(3, 5);
+        } catch (PurchaseException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
